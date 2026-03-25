1552171989dcbc48d56311495b4c5fc5c604c821
0maru <0maru.dev@gmail.com>
2026-03-25 15:00:04 +0900

    ou formula を v0.4.0 に更新

--- Formula/ou.rb
+++ Formula/ou.rb
@@ -1,8 +1,8 @@
 class Ou < Formula
   desc "Git worktree management CLI tool"
   homepage "https://github.com/0maru/ou"
-  url "https://github.com/0maru/ou/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "66a603f30c07491e6db625b3cee8e0d233fe1ef337959c0187db7d2489a742a7"
+  url "https://github.com/0maru/ou/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "ba61767c6e6cc7e2ff136ca11fa73b277dc5d7291e5bfaf86abf501a2b0b4ed3"
   license "MIT"
   head "https://github.com/0maru/ou.git", branch: "main"
 
